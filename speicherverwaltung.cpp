7e0f8199a35491df5fd7546b7e62b2e18b023c94
KLuka <klukasocial+github@gmail.com>
2013-12-15 22:31:09 +0100

    fix std::hex and use std::dec. And fix placement of return in myfree.

--- speicherverwaltung.cpp
+++ speicherverwaltung.cpp
@@ -67,22 +67,23 @@ void *mymalloc(unsigned int size /*requested size in bytes*/, int line) {
 void myfree(void *addressToDelete) {
 	for(std::list<memorySegment>::iterator it = memSegments.begin(); it != memSegments.end(); ++it){
 		if (it->memAdress == (unsigned int)addressToDelete) {
+			totalAlloc = totalAlloc - it->size;
 			if (it == memSegments.begin()) { // has only one neighbor
 				it->isAllocated = false;
 				if (std::next(it, 1)->isAllocated == false) {
 					//merge two free blocks together
 					it->size += std::next(it, 1)->size;					
-					memSegments.erase(std::next(it, 1));
-					return;
-				}				
+					memSegments.erase(std::next(it, 1));					
+				}
+				return;
 			}
-			else if(it == memSegments.end()) {
+			else if(it == memSegments.end()) { // has only one neighbor
 				it->isAllocated = false;
 				if (std::prev(it, 1)->isAllocated == false) {
 					std::prev(it, 1)->size += it->size; // the previous shall become the head of the freeMemSeg
-					memSegments.erase(it);
-					return;
+					memSegments.erase(it);					
 				}
+				return;
 			}
 			else
 			{
@@ -94,40 +95,50 @@ void myfree(void *addressToDelete) {
 						memSegments.erase(std::next(it, 1) );
 					}
 					memSegments.erase(it);
-					return;
 				}
 				else if (std::next(it, 1)->isAllocated == false) { // case a1 a2 f3 and we want to free a2
 					it->size += std::next(it, 1)->size;
-					memSegments.erase(std::next(it, 1) );
-					return;
+					memSegments.erase(std::next(it, 1) );					
 				}
+				return;
 			}
 		}
 	}
 }
 
 void mystatus(void){
+	cout<< "######## status of memory #######" << endl;
 	cout<< "start: " <<std::hex<< memBlockBeginning<< " End: " <<std::hex<<memBlockBeginning+(totalMemory*8)<< endl;
-	cout<< "totalmem: " << std::hex<< totalMemory << " (" << totalMemory << ")" << endl;
-	cout<< "Total allocated: " << std::hex<<totalAlloc<< " (" << totalAlloc << ")" << endl;
+	cout<< "totalmem: " << std::hex<< totalMemory << std::dec << " (" << totalMemory << ")" << endl;
+	cout<< "Total allocated: " << std::hex<<totalAlloc<< " (" << std::dec << totalAlloc << ")" << endl;
 	// iterate through allocations:
 	int i = 0; // for numbering the allocations
 	for(std::list<memorySegment>::iterator it = memSegments.begin(); it != memSegments.end(); ++it){
 		if (it->isAllocated == true) {
 			cout << "Allocation " << i << ":" << endl;
-			cout << "start: " <<std::hex<<it->memAdress << "end: " <<std::hex<< it->memAdress+(it->size*8) << endl;
-			cout << "mymalloc(" <<it->size << "); in line " <<it->programline << endl;
+			cout << "Start: " <<std::hex<<it->memAdress << " End: " <<std::hex<< it->memAdress+(it->size*8) << endl;
+			cout << "mymalloc(" <<std::dec<<it->size << "); in line " <<it->programline << endl;
 			++i;
 		}		
 	}
+	cout << endl;
 }
 
 int main(int argc, char* argv[])
 {
-	myinit(2048, FIRST_FIT);
+	myinit(4096, FIRST_FIT);
 
 	void *p1 = mymalloc(512, __LINE__);
 	void *p2 = mymalloc(1024, __LINE__);
+	void *p3 = mymalloc(1024, __LINE__);
+
+	mystatus();
+
+	cout << "freeing memory...." << endl << endl;
+	myfree(p1);
+	myfree(p3);
+
+	mystatus();
 
 	cout << "did it work?" << endl;
 
